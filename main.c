c625f36094fba13fe732fbcda836078d8d39290d
lonerr <glonerr@gmail.com>
2018-08-27 19:01:12 +0800

    update

--- main.c
+++ main.c
@@ -1,5 +1,13 @@
 #include "libavutil/mem.h"
+#include "libavutil/timer.h"
+#include "libavutil/log.h"
+#include "libavutil/adler32.h"
+#include "libavutil/random_seed.c"
 #include <stdio.h>
+
+#define LEN 7001
+
+static volatile int checksum;
 
 int main(int argc, char **argv)
 {
@@ -30,11 +38,35 @@ int main(int argc, char **argv)
     // if (!b)
     //     return 1;
 
-    void* b = av_malloc(10);
+    void *b = av_malloc(10);
     // int* cp = av_malloc(sizeof(int));
     // *cp = 3;
     // printf("0x%x,0x%x,%d",b,cp,*cp);
     av_free(b);
     // printf("0x%x,0x%x,%d",b,cp,*cp);
-    return 1;
+
+    int i;
+    uint8_t data[LEN];
+
+    av_log_set_level(AV_LOG_DEBUG);
+
+    for (i = 0; i < LEN; i++)
+        data[i] = ((i * i) >> 3) + 123 * i;
+
+    if (argc > 1 && !strcmp(argv[1], "-t"))
+    {
+        for (i = 0; i < 1000; i++)
+        {
+            START_TIMER;
+            checksum = av_adler32_update(1, data, LEN);
+            STOP_TIMER("adler");
+        }
+    }
+    else
+    {
+        checksum = av_adler32_update(1, data, LEN);
+    }
+
+    av_log(NULL, AV_LOG_DEBUG, "%X (expected 50E6E508)\n", checksum);
+    return checksum == 0x50e6e508 ? 0 : 1;
 }
