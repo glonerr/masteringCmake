56ee7547266ec2aa8f969b749ad2107f99f5634b
lonerr <glonerr@gmail.com>
2018-10-30 19:02:23 +0800

    update

--- make_build.sh
+++ make_build.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 cd build && rm -rf * && cmake ../ && make -j
 adb push libavutil/libavutil.so /data/local/tmp
-for i in $(ls libavutil/tests); do
+for i in adler32 aes aes_ctr audio_fifo avstring base64 blowfish bprint cast5 camellia color_utils cpu crc des dict display encryption_info error eval file fifo hash hmac hwdevice integer imgutils lfg lls log md5 murmur3 opt pca parseutils pixdesc pixelutils pixfmt_best random_seed rational ripemd sha sha512 softfloat tree twofish utf8 xtea tea; do
 	adb push libavutil/tests/$i /data/local/tmp
 	echo "******************************************************************"
 	echo "*********************** test $i *************************"
 	adb shell export "LD_LIBRARY_PATH=/data/local/tmp&&/data/local/tmp/$i"
 	echo "******************************************************************"
 done
-# file libavutil/main && adb push libavutil/libavutil.so libavutil/main /data/local/tmp && adb shell export "LD_LIBRARY_PATH=/data/local/tmp&&/data/local/tmp/main"
